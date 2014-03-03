c704216096d32513dfcf2ce3107e5893b8b9871e
Dominik Guzei <dominik.guzei@gmail.com>
2014-03-03 18:25:16 +0100

    make app secure and show how to publish generated collections

--- static-pages.js
+++ static-pages.js
@@ -1,8 +1,14 @@
 if (Meteor.isClient) {
 
+  // subscribe to the generated data
+  Meteor.subscribe('pages');
+  Meteor.subscribe('products');
+
+  // get a reference to the generated collections
   var Pages = new Meteor.Collection('pages');
   var Products = new Meteor.Collection('products');
 
+  // use the collections like normally
   Template.pages.helpers({
     pages: Pages.find()
   });
@@ -15,8 +21,17 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
 
+    // Tell electrostatic to generate the collections and items
     Electrostatic.generate(function() {
-      // do something with the generated collections here
+
+      // publish the generated collections for the client
+      Meteor.publish('pages', function() {
+        return Electrostatic.pages.find();
+      });
+
+      Meteor.publish('products', function() {
+        return Electrostatic.products.find();
+      });
     });
 
   });
